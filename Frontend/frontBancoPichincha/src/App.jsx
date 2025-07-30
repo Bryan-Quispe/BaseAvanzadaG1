9fac4753b192c20199011e901cf9345f563bbf2b
GabrielLopez <gmlopez11@espe.edu.ec>
2025-07-30 11:01:12 -0500

    menu mas bonito 2

--- Frontend/frontBancoPichincha/src/App.jsx
+++ Frontend/frontBancoPichincha/src/App.jsx
@@ -1,27 +1,19 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login.jsx';
-=======
 import { useState } from 'react'
 import './App.css'
 import Login from  './pages/Login.jsx';
 import { Menu } from './pages/Menu.jsx';
->>>>>>> 38fd49dafd7ff18aafa59fa2401f4cc3146b7192
 
 function App() {
   return (
-<<<<<<< HEAD
+
       <Routes>
         <Route path="/" element={<Login />} />
       </Routes>
   );
-=======
-    <>
-    <Menu />
-    </>
-  )
->>>>>>> 38fd49dafd7ff18aafa59fa2401f4cc3146b7192
+
 }
 
 export default App;

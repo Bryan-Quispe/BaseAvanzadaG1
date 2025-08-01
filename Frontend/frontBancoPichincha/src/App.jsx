4acf12ba7cf194cedd6848fbe6f45d0b908e83d8
unknown <matemedranda15@gmail.com>
2025-08-01 10:54:54 -0500

    V1.2.0 Adding the login

--- Frontend/frontBancoPichincha/src/App.jsx
+++ Frontend/frontBancoPichincha/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Login from './pages/Login.jsx';
 import { useState } from 'react'
 import { Menu } from './pages/Menu.jsx';
+import Register from './pages/Register.jsx';
 
 function App() {
   return (
 
       <Routes>
         <Route path="/" element={localStorage.getItem("token") === null ? <Login /> : <Menu />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
   );
 
---
+++ Frontend/frontBancoPichincha/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Register() {
+    return(
+        <>
+            <h1>Registro</h1>
+        </>
+    );
+}
+
+export default Register;
